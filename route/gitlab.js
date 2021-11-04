8dd2a491792faebd912ba3f3d536a0301586f03e
Jeremy Mohr <jmohr@atb.com>
2021-11-04 16:25:22 -0700

    Added getting comments from gitlab, refactored some

--- route/gitlab.js
+++ route/gitlab.js
@@ -6,7 +6,6 @@ const route = express.Router();
 //TODO different projects and shit
 const GITLAB_PROJECT_ID=8920796
 
-
 route.get("/", (req, res) => {
   console.log("here");
   res.send("getting");
@@ -17,32 +16,71 @@ route.post("/", (req, res) => {
   console.log(req, "lklk")
 });
 
-route.get("/test", async (req, res) => {
-  
-  console.log("Making a test request");
-  try{
 
-    const {data} = await axios.get('https://gitlab.com/api/v4/projects/' + GITLAB_PROJECT_ID + '/merge_requests',
-    {
-      headers: {
+function getBaseUrl(){
+    return 'https://gitlab.com/api/v4/projects/' + GITLAB_PROJECT_ID;
+}
+
+function getHeaders(){
+    return {
         'PRIVATE-TOKEN': process.env.GITLAB_TOKEN,
-      },
-      params: {
-        state: 'opened',
-        order_by: 'updated_at',
-      }
+    }
+}
+
+async function request(verb, endpoint, params) {
+    try {
+        var { data } = await (axios({
+            method: verb,
+            url: getBaseUrl() + endpoint,
+            headers: getHeaders(),
+            params: params
+        }))
+        console.log("OH YES: " + data);
+        return { data };
+    } catch (error) {
+        console.log("OH NO " + error);
+        if (error?.response?.data?.error) {
+            console.log("OH NO " + error.response.data.error);
+        }
+    }
+}
+
+async function getMergeRequests(){
+    return request('get', '/merge_requests', {
+          state: 'opened',
+          order_by: 'updated_at',
+        });
+}
+
+async function getComments(iid){
+    return request('get', '/merge_requests/' + iid + '/notes', {
+    	sort:'desc',
+    	order_by:'updated_at'
     })
-  
-    console.log("OH YES: got data");
+}
 
-    return res.send({data});
+route.get("/test", async (req, res) => {
+  response = await getMergeRequests();
+  var output = "";
 
-  } catch(error) {
-    console.log("OH NO " + error);
-    console.log(Object.keys(error));
-    console.log("OH NO " + error.response.data.error);
-  };
-
+  for (const id of response.data.keys()){
+    var item = response.data[id]
+    output = output + "</br></br></br>" + item.title +"</br>";
+    if(item.iid){
+      console.log("IID " + item.iid + " LETS GO");
+      comments = await getComments(item.iid);
+      console.log(comments);
+      for (const comment of comments.data){
+        if(comment.system){
+          continue;
+        }
+      	var user = (comment?.author?.name || "someone?");
+      	output = output + "<br>" + user + ": " + comment.body;
+      }
+    }
+  }
+  console.log(output);
+  return res.send(output);
 });
 
 
