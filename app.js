c37babc0e171c52204e55e6c8d731b2b8a446b38
zwe <zwemsoe@gmail.com>
2021-11-04 12:01:35 -0700

    heroku slack

--- app.js
+++ app.js
@@ -1,22 +1,20 @@
-const express = require("express");
-
-const app = express();
-
-// parse json request body
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-
-app.get("/", (req, res) => {
-  res.send("Index");
+const { App } = require("@slack/bolt");
+require("dotenv").config();
+// Initializes your app with your bot token and signing secret
+const app = new App({
+  token: process.env.SLACK_BOT_TOKEN,
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+  socketMode: false, // enable the following to use socket mode
+  appToken: process.env.APP_TOKEN,
 });
 
-// send back a 404 error for any unknown api request
-app.use((req, res, next) => {
-  res.send("Not found");
-});
-
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-  console.log(`Example app listening at http://localhost:${PORT}`);
-});
+(async () => {
+  try {
+    const port = 3000;
+    // Start your app
+    await app.start(process.env.PORT || port);
+    console.log(`⚡️ Slack Bolt app is running on port ${port}!`);
+  } catch (e) {
+    console.log(e);
+  }
+})();
