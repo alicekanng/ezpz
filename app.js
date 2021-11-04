08bb2f2f218e3a7a49ec1e66f3b4167629bb0e86
Alice Kang <akang@atb.com>
2021-11-04 12:41:29 -0700

    test

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+app.get("/healthcheck", (req, res) => {
+  res.send(":)");
+})
+
 app.get("/", (req, res) => {
   res.send("Index");
   const { App } = require("@slack/bolt");
