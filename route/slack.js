ae1d1edeae201d79d5bc2d68115cdb553211edf4
Keshav <kgupta@atb.com>
2021-11-04 15:25:45 -0700

    awaitiing

--- route/slack.js
+++ route/slack.js
@@ -2,11 +2,11 @@ const express = require("express");
 const axios = require("axios");
 const route = express.Router();
 
-route.get("/", (req, res) => {
-    axios.post("https://hooks.slack.com/services/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", {
+route.get("/", async (req, res) => {
+    await axios.post("https://hooks.slack.com/services/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", {
         "text": "chris is gac"
     });
-    res.send("jay");
+    res.send("await jay");
 });
 
 module.exports = route;
