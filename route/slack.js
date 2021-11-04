f6a7c7772fe9076d38bfd1a03ae2b1be925b5eb2
zwe <zwemsoe@gmail.com>
2021-11-04 15:52:25 -0700

    hehe

--- route/slack.js
+++ route/slack.js
@@ -3,15 +3,19 @@ const axios = require("axios");
 const route = express.Router();
 
 route.get("/", async (req, res) => {
-    await axios.post(process.env.SLACK_WEBHOOK_URL, {
-        "text": "chris is gac"
-    }, {
-        headers: {
-            'Content-Type': 'application/json',
-        }
-    });
+  await axios.post(
+    process.env.SLACK_WEBHOOK_URL,
+    {
+      text: "chris is gac",
+    },
+    {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  );
 
-    res.send("await jay");
+  res.send("await jay");
 });
 
 module.exports = route;
