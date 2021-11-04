d4933e7f26b2463adc8632386283eca22099cbef
zwe <zwemsoe@gmail.com>
2021-11-04 12:16:16 -0700

    fixing app

--- app.js
+++ app.js
@@ -4,15 +4,15 @@ if (process.env.NODE_ENV === "development") require("dotenv").config();
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
   signingSecret: process.env.SLACK_SIGNING_SECRET,
-  socketMode: false, // enable the following to use socket mode
+  socketMode: true, // enable the following to use socket mode
   appToken: process.env.APP_TOKEN,
 });
 
 (async () => {
   try {
-    const port = 3000;
+    const port = process.env.PORT || 3000;
     // Start your app
-    await app.start(process.env.PORT || port);
+    await app.start(port);
     console.log(`⚡️ Slack Bolt app is running on port ${port}!`);
   } catch (e) {
     console.log(e);
