eb9840456b70cd3853d7339a22d801ae2bc34276
Keshav <kgupta@atb.com>
2021-11-04 15:08:28 -0700

    Added something man

--- route/slack.js
+++ route/slack.js
@@ -1,7 +1,11 @@
 const express = require("express");
+const axios = require("axios");
 const route = express.Router();
 
 route.get("/", (req, res) => {
+    axios.post("https://hooks.slack.com/services/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", {
+        "text": "chris is gac"
+    });
     res.send("this is a string");
 });
 
