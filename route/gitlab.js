91d0bc751b508a31b416044d3bad5d05fc52d0ea
Alice Kang <akang@atb.com>
2021-11-04 16:48:38 -0700

    fasfksj

--- route/gitlab.js
+++ route/gitlab.js
@@ -10,26 +10,11 @@ route.get("/", (req, res) => {
   res.send("getting");
 });
 
-<<<<<<< HEAD
-route.post("/", async (req, res) => {
-  const type = req.get('X-Gitlab-Event')
-  const message = formatMessage(type, req.body)
-  console.log(message)
-  try{
-    await sendMessageToGroup(message)
-    return res.status(200)
-  } catch(err){
-    console.log(err);
-    return res.status(400)
-  }
-  
-=======
 route.post("/", (req, res) => {
   const type = req.get("X-Gitlab-Event");
   const message = formatMREventMessage(type, req.body);
   console.log(message);
   sendMessageToGroup(message);
->>>>>>> 87fd94a087c0bcc68be5acde626148986e7f3423
 });
 
 route.get("/test", async (req, res) => {
