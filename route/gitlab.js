434f4b06a2e3a32a5416a6c346aabe3c87e37fb4
zwe <zwemsoe@gmail.com>
2021-11-05 11:04:27 -0700

    cron test

--- route/gitlab.js
+++ route/gitlab.js
@@ -1,11 +1,11 @@
 const axios = require("axios");
 const express = require("express");
 const route = express.Router();
-const { formatMessage } = require('../formater')
-const { sendMessageToGroup } = require('../slackHelper')
+const { formatMessage } = require("../formater");
+const { sendMessageToGroup } = require("../slackHelper");
 
 //TODO different projects and shit
-const GITLAB_PROJECT_ID=8920796
+const GITLAB_PROJECT_ID = 8920796;
 
 route.get("/", (req, res) => {
   console.log("here");
@@ -13,88 +13,126 @@ route.get("/", (req, res) => {
 });
 
 route.post("/", (req, res) => {
-  const type = req.get('X-Gitlab-Event')
-  const message = formatMessage(type, req.body)
-  console.log(message)
-  sendMessageToGroup(message)
+  const type = req.get("X-Gitlab-Event");
+  const message = formatMessage(type, req.body);
+  console.log(message);
+  sendMessageToGroup(message);
 });
 
-
-function getBaseUrl(){
-    return 'https://gitlab.com/api/v4/projects/' + GITLAB_PROJECT_ID;
+function getBaseUrl() {
+  return "https://gitlab.com/api/v4/projects/" + GITLAB_PROJECT_ID;
 }
 
-function getHeaders(){
-    return {
-        'PRIVATE-TOKEN': process.env.GITLAB_TOKEN,
-    }
+function getHeaders() {
+  return {
+    "PRIVATE-TOKEN": process.env.GITLAB_TOKEN,
+  };
 }
 
 async function request(verb, endpoint, params) {
-    try {
-        var { data } = await (axios({
-            method: verb,
-            url: getBaseUrl() + endpoint,
-            headers: getHeaders(),
-            params: params
-        }))
-        console.log("OH YES: " + data);
-        return { data };
-    } catch (error) {
-        console.log("OH NO " + error);
-        if (error?.response?.data?.error) {
-            console.log("OH NO " + error.response.data.error);
-        }
+  try {
+    var { data } = await axios({
+      method: verb,
+      url: getBaseUrl() + endpoint,
+      headers: getHeaders(),
+      params: params,
+    });
+    console.log("OH YES: " + data);
+    return { data };
+  } catch (error) {
+    console.log("OH NO " + error);
+    if (error?.response?.data?.error) {
+      console.log("OH NO " + error.response.data.error);
     }
+  }
 }
 
-async function getMergeRequests(){
-    //https://docs.gitlab.com/ee/api/merge_requests.html#list-merge-requests
-    return request('get', '/merge_requests', {
-          state: 'opened',
-          order_by: 'updated_at',
-        });
+async function getMergeRequests() {
+  //https://docs.gitlab.com/ee/api/merge_requests.html#list-merge-requests
+  return request("get", "/merge_requests", {
+    state: "opened",
+    order_by: "updated_at",
+  });
 }
 
-async function getDiscussions(iid){
-    //https://docs.gitlab.com/ee/api/discussions.html#list-project-issue-discussion-items
-    return request('get', '/merge_requests/' + iid + '/discussions', {
-        sort:'desc',
-        order_by:'updated_at'
-    })
+async function getDiscussions(iid) {
+  //https://docs.gitlab.com/ee/api/discussions.html#list-project-issue-discussion-items
+  return request("get", "/merge_requests/" + iid + "/discussions", {
+    sort: "desc",
+    order_by: "updated_at",
+  });
 }
 
 route.get("/test", async (req, res) => {
-  response = await getMergeRequests();
-  var output = "";
-
-  //Go through open MRS
-  for (const id of response.data.keys()){
-    var item = response.data[id]
-    output = output + "</br></br></br>" + item.title +"</br>";
-    
-    //use the issue ID
-    if(item.iid){
-      console.log("IID " + item.iid + " LETS GO");
-      //We use the issue id to pull discussions for that MR
-      discussions = await getDiscussions(item.iid);
-      console.log(JSON.stringify(discussions));
-      for (const discussion of discussions.data){
-          output = output + "DISCUSSION:</br>"
-          for (const comment of discussion.notes){
-          //I don't know if this shows up for /discussions but if you get /notes you'll also get notes from the system that say "Kevin approved" or whatever
-          if (comment.system){
-            continue;
-          }
-          output = output + "<br>" + (comment?.author?.name || "someone?") + ": " + comment.body;
-        }
-        output = output + "END DISCUSSION:</br></br></br>"
-      }
-    }
-  }
-  console.log(output);
-  return res.send(output);
+  const response = await getMergeRequests();
+  response.data.forEach((mr) => {
+    const {
+      title = "",
+      description = "",
+      author: { name = "", username = "", avatar_url = "" },
+      web_url = "",
+      source_branch = "",
+      target_branch = "",
+      assignees = [],
+      merge_status = "",
+      approvals_before_merge = 0,
+    } = mr;
+    const message = {
+      attachments: [
+        {
+          fallback: `${name} has a merge request to helpful-stuff`,
+          color: "#36a64f",
+          pretext: `:envelope_with_arrow: ${name} has a merge request to helpful-stuff`,
+          author_name: name,
+          author_link: `https://gitlab.com/${username}`,
+          author_icon: avatar_url,
+          title,
+          title_link: web_url,
+          text: description,
+          fields: [
+            {
+              title: "Source:",
+              value: `<${web_url}/tree/${source_branch}|${source_branch}>`,
+              short: true,
+            },
+            {
+              title: "Target:",
+              value: `<${web_url}/tree/${target_branch}|${target_branch}>`,
+              short: true,
+            },
+            {
+              title: "Assignee:",
+              value: assignees[0]
+                ? assignees[0].name
+                : "No one is assigned to this MR",
+              short: false,
+            },
+            {
+              title: "Merge Status:",
+              value:
+                merge_status === "can_be_merged"
+                  ? "This merge request can be merged!"
+                  : `Need ${approvals_before_merge} more approvals`,
+              short: false,
+            },
+          ],
+          actions: [
+            {
+              name: "sub",
+              text: "Subscribe",
+              type: "button",
+              value: "subscribe",
+            },
+          ],
+          footer: "Gitlab Webhook",
+          footer_icon: avatar_url,
+          ts: Date.now(),
+        },
+      ],
+    };
+    sendMessageToGroup(message);
+  });
+  return res.status(200);
 });
-
 
 module.exports = route;
