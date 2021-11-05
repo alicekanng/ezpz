d423b054f1675fb721d315e264d2e0873d064043
zwe <zwemsoe@gmail.com>
2021-11-05 12:25:48 -0700

    not resolved

--- gitlabHelper.js
+++ gitlabHelper.js
@@ -49,7 +49,9 @@ async function getDiscussions(iid) {
 function getOpenThreads(discussionsResponse) {
   let openThreads = [];
   discussionsResponse.data.forEach((data) => {
-    openThreads.push(...data.notes.filter((note) => note.resolvable === true));
+    openThreads.push(
+      ...data.notes.filter((note) => note.resolvable === true && !note.resolved)
+    );
   });
   return openThreads;
 }
